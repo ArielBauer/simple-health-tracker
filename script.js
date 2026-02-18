a81931d4d9033efde3ac6ff55ff1195f5a05f1a0
LaylaPrates <laylaprates.21@gmail.com>
2026-02-18 11:34:59 -0300

    feat: new 'if else' statement checks if the height value contains a decimal point, ensuring correct calculation, and sends a alert if it doesn't

--- script.js
+++ script.js
@@ -5,14 +5,22 @@ btn.addEventListener('click', function() {
     const weight = document.getElementById('weight').value;
     const height = document.getElementById('height').value;
 
-    if (weight > 0 && height > 0) {
-        
-        
-        const bmi = weight / (height * height);
+    if(height.includes(".")){
+        if (weight > 0 && height > 0) {
+            
+            
+            const bmi = weight / (height * height);
 
+            
+            document.getElementById('bmi-value').innerText = `${bmi.toFixed(2)}`;
+        } else {
+
+            alert("Please fill in all fields!");
+
+        };
+    }else{
+
+        alert("Please include a comma or a period in the heigth!");
         
-        document.getElementById('bmi-value').innerText = `${bmi.toFixed(2)}`;
-    } else {
-        alert("Please fill in all fields!");
     };
 });
