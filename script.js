e76faded351519e650b6ec9434add639ab0347bd
LaylaPrates <laylaprates.21@gmail.com>
2026-02-18 11:57:31 -0300

    feat: adding a status message depending on the BMI

--- script.js
+++ script.js
@@ -13,6 +13,22 @@ btn.addEventListener('click', function() {
 
             
             document.getElementById('bmi-value').innerText = `${bmi.toFixed(2)}`;
+
+            const result = document.getElementById("description");
+            if (bmi < 18.5) {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you are underweight.`;
+            } else if (bmi < 25) {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you have a normal weight.`;
+            } else if (bmi < 30) {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you are overweight.`;
+            } else if (bmi < 35) {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you have obesity class I.`;
+            } else if (bmi < 40) {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you have obesity class II.`;
+            } else {
+                result.innerHTML = `With a BMI of ${bmi.toFixed(2)} you have obesity class III.`;
+            }
+
         } else {
 
             alert("Please fill in all fields!");
@@ -21,6 +37,6 @@ btn.addEventListener('click', function() {
     }else{
 
         alert("Please include a comma or a period in the heigth!");
-        
+
     };
 });
